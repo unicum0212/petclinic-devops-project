98c6b0ad915c01ad245d2fec835fa264795ab846
Dima Tsykalenko <dmitriy.tsykalenko@gmail.com>
2023-01-15 12:15:35 +0200

    Changed Launch Conf

--- terraform/env_1/launch-conf.sh
+++ terraform/env_1/launch-conf.sh
@@ -6,6 +6,6 @@ apt install maven -y
 apt install dos2unix -y
 mkdir /home/ubuntu/app
 chmod 777 /home/ubuntu/app
-apt install apache2 -y
-echo Hello > /var/www/html/index.html
-systemctl reload apache2
+#apt install apache2 -y
+#echo Hello > /var/www/html/index.html
+#systemctl reload apache2
